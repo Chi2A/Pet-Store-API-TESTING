1cf01fe959f3690d5320ed993c98480e3ce01b65
Chi2A <154117631+Chi2A@users.noreply.github.com>
2025-10-15 19:34:12 -0500

    updated tests

--- tests/user-APIs.spec.ts
+++ tests/user-APIs.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from "@playwright/test";
 import { faker } from "@faker-js/faker";
 import { z } from "zod";
+import { getAPI, postAPI, putAPI, deleteAPI } from "../utils/apiCallHelper";
 
 test.describe("User API Tests", () => {
     const BASE_URL = `${process.env.BASE_URL}${process.env.API_VERSION}`;
@@ -14,66 +15,59 @@ test.describe("User API Tests", () => {
        phone: faker.phone.number(),
        userStatus: faker.number.int({ min: 1, max: 10 })
      };
+    
+    const expectedCreateUserResponseSchema = z.object({
+      code: z.literal(200),
+      type: z.literal("unknown"),
+      message: z.literal(createUserRequestBody.id.toString()),
+    });
+
+     const expectedGetUserByUserNameResponseSchema = z.object({
+       id: z.number(),
+       username: z.string(),
+       firstName: z.string().optional(),
+       lastName: z.string().optional(),
+       email: z.string().email().optional(),
+       password: z.string().optional(),
+       phone: z.string().optional(),
+       userStatus: z.number().optional(),
+     });
+    
+     const userName = createUserRequestBody.username;
+     const expectedDeleteUserByUserNameResponseSchema = z.object({
+       code: z.literal(200),
+       type: z.literal("unknown"),
+       message: z.literal(createUserRequestBody.username),
+     });
       test("Create a new user", async ({ request }) => {
-       const userName = createUserRequestBody.username;
-        const createUserResponse = await request.post(`${BASE_URL}/user`, {
-          data: createUserRequestBody,
-        });
-        
-          expect(createUserResponse.status()).toBe(200);
-          const expectedCreateUserResponseSchema = z.object({
-            code: z.literal(200),
-            type: z.literal("unknown"),
-            message: z.literal(createUserRequestBody.id),
-          });
-        const createUserResponseBody = await createUserResponse.json();
-        expectedCreateUserResponseSchema.parse(createUserResponseBody);
+
+          await postAPI(
+              request,
+              `${BASE_URL}/user`,
+              createUserRequestBody,
+              200,
+              expectedCreateUserResponseSchema
+          );
       });
     
     test("Get user by username", async ({ request }) => {
         const userName = createUserRequestBody.username;
-        let getUserResponse;
-        for(let i=0; i<5; i++){
-          getUserResponse = await request.get(`${BASE_URL}/user/${userName}`);
-            if (getUserResponse.status() === 200) break;
-            console.log(`Attempt ${i + 1} failed. Retrying...`);
-        }
-        const getUserByUserNameResponse = await request.get(`${BASE_URL}/user/${userName}`);
-        
-        expect(getUserByUserNameResponse.status()).toBe(200);
        
-
-        const expectedGetUserByUserNameResponseSchema = z.object({
-          id: z.number(),
-          username: z.string(),
-          firstName: z.string().optional(),
-          lastName: z.string().optional(),
-          email: z.string().email().optional(),
-          password: z.string().optional(),
-          phone: z.string().optional(),
-          userStatus: z.number().optional(),
-        });
-
-         const actualGetUserByUserNameResponseBody =
-           await getUserByUserNameResponse.json();
-        expectedGetUserByUserNameResponseSchema.parse(actualGetUserByUserNameResponseBody);
-
-
+        await getAPI(
+          request,
+          `${BASE_URL}/user/${userName}`,
+          200,
+          expectedGetUserByUserNameResponseSchema
+        );
     });
 
     test("Delete user by username", async ({ request }) => {
-        const userName = createUserRequestBody.username;
-        const deleteUserByUserNameResponse = await request.delete(`${BASE_URL}/user/${userName}`);
-        expect(deleteUserByUserNameResponse.status()).toBe(200);
-        const expectedDeleteUserByUserNameResponseSchema = z.object({
-          code: z.literal(200),
-          type: z.literal("unknown"),
-          message: z.literal(createUserRequestBody.username),
-        });
-        const deleteUserByUserNameResponseBody =
-          await deleteUserByUserNameResponse.json();
-        expectedDeleteUserByUserNameResponseSchema.parse(deleteUserByUserNameResponseBody);
     
-    
+       await deleteAPI(
+          request,
+          `${BASE_URL}/user/${userName}`,
+          200,
+          expectedDeleteUserByUserNameResponseSchema
+        );
     }); 
 });
