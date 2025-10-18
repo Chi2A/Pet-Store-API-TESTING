080cba43bd36734d74b5746c155bafae1fea7d74
Chi2A <154117631+Chi2A@users.noreply.github.com>
2025-10-17 20:58:05 -0500

    fix

--- tests/returnsPetInventoriesByStatus.spec.ts
+++ tests/returnsPetInventoriesByStatus.spec.ts
@@ -2,7 +2,7 @@ import { test, expect ,request} from "@playwright/test";
 import { z } from "zod";
 import { getAPI, postAPI } from "../utils/apiCallHelper";  
 
-const BASE_URL = `${process.env.BASE_URL}${process.env.API_VERSION}/store/inventory`
+const BASE_URL = `${process.env.BASE_URL}${process.env.API_VERSION}`
   const expectedInventoryResponseSchema = z
     .record(z.string(), z.number())
     .refine((data: Record<string, number>) => Object.keys(data).length === 14, {
@@ -13,7 +13,7 @@ test("should return pet inventories with 14 items", async ({ request }) => {
 
 const response = await getAPI(
       request,
-      BASE_URL,
+      `${BASE_URL}/store/inventory`,
       200,
       expectedInventoryResponseSchema
     );
