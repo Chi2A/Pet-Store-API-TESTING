12fe890a8c9d26d0ccdd827ae993871937578765
Chi2A <154117631+Chi2A@users.noreply.github.com>
2025-10-17 20:55:48 -0500

    test

--- tests/returnsPetInventoriesByStatus.spec.ts
+++ tests/returnsPetInventoriesByStatus.spec.ts
@@ -1,19 +1,20 @@
-import { test, expect } from "@playwright/test";
+import { test, expect ,request} from "@playwright/test";
 import { z } from "zod";
+import { getAPI, postAPI } from "../utils/apiCallHelper";  
 
-test("should return pet inventories with 14 items", async ({ request }) => {
-  const response = await request.get(
-    `${process.env.BASE_URL}${process.env.API_VERSION}/store/inventory`
-  );
-
-  expect(response.status()).toBe(200);
-  const data = await response.json();
-  const numberOfKeys = Object.keys(data).length;
-  expect(numberOfKeys).toBe(14);
-
+const BASE_URL = `${process.env.BASE_URL}${process.env.API_VERSION}/store/inventory`
   const expectedInventoryResponseSchema = z
     .record(z.string(), z.number())
     .refine((data: Record<string, number>) => Object.keys(data).length === 14, {
       message: "Response must have exactly 14 keys",
     });
+
+test("should return pet inventories with 14 items", async ({ request }) => {
+
+const response = await getAPI(
+      request,
+      BASE_URL,
+      200,
+      expectedInventoryResponseSchema
+    );
 });
